61c43c84d272e439ee57096bdf0bda0f1971663b
desarrollo <jsaavedra.tp@gmail.com>
2021-06-03 22:03:10 -0400

    avances jorge

--- resources/views/auth/registration.blade.php
+++ resources/views/auth/registration.blade.php
@@ -67,8 +67,25 @@
                             </div>
 
                             <div class="form-group mb-3">
-                                <input type="text" placeholder="Region" id="region" class="form-control" name="region"
+                                <select placeholder="Region" id="region" class="form-control" name="region"
                                     required autofocus>
+                                    <option value="">Región</option>
+                                    <option value="Región de Arica y Parinacota">Región de Arica y Parinacota</option>
+                                    <option value="Región de Tarapacá">Región de Tarapacá</option>
+                                    <option value="Región de Antofagasta">Región de Antofagasta</option>
+                                    <option value="Región de Atacama">Región de Atacama</option>
+                                    <option value="Región de Coquimbo<">Región de Coquimbo</option>
+                                    <option value="Región de Valparaíso">Región de Valparaíso</option>
+                                    <option value="Región Metropolitana de Santiago">Región Metropolitana de Santiago</option>
+                                    <option value="Región del Libertador General Bernardo O’Higgins">Región del Libertador General Bernardo O’Higgins</option>
+                                    <option value="Región del Maule">Región del Maule</option>
+                                    <option value="Región del Ñuble">Región del Ñuble</option>
+                                    <option value="Región del Biobío<">Región del Biobío</option>
+                                    <option value="Región de La Araucanía">Región de La Araucanía</option>
+                                    <option value="Región de Los Ríos<">Región de Los Ríos</option>
+                                    <option value="Región de Los Lagos">Región de Los Lagos</option>
+                                    <option value="Región de Aysén del General Carlos Ibáñez del Campo">Región de Aysén del General Carlos Ibáñez del Campo</option>
+                                    <option value="Región de Magallanes y la Antártica Chilena">Región de Magallanes y la Antártica Chilena</option> </select>
                                 @if ($errors->has('region'))
                                 <span class="text-danger">{{ $errors->first('region') }}</span>
                                 @endif
